094cfc8be68c761b76135c2cb6ccc07540f93e49
YorklinPunk <zetita159@gmail.com>
2025-09-15 23:18:31 -0500

    Creando función de trap y limpieza de recursos

--- src/main.sh
+++ src/main.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 set -euo pipefail
-PORT="${1:-8080}"
-MESSAGE="La aplicación está funcionando en el puerto"
+
+#Variables de entorno con valores por defecto
+: "${PORT:=${1:-8080}}"
+: "${MESSAGE:="La aplicación está funcionando en el puerto"}"
+
+# Función de limpieza
+cleanup() {
+    echo "Apagando servidor en puerto $PORT..."
+}
+trap cleanup EXIT INT TERM
+
+
 echo "servidor simple con el puerto $PORT"
 while true; do
     echo -e "HTTP/1.1 200 OK\n\n$MESSAGE" | nc -l -p "$PORT"
